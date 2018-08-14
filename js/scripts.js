e18ba6c8bb5e4256ecd1c54bca2667ca9a4e1fc1
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-14 15:17:17 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".clickable").click(function(){
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+
+/*  $("h1").click(function(){
+    alert("This is a header");
+  });
+  $("p").click(function(){
+    alert("This is a paragraph");
+  });
+  $("img").click(function(){
+    alert("this is an img");
+  });*/
+});
